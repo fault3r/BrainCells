f84cc1980084b1af84157891bc12cd196f510040
Hamed Damavandi <hamed.damaavandi@gmail.com>
2025-01-09 22:46:22 +0330

    bug fix login page

--- BrainCells.Presentation/wwwroot/js/site.js
+++ BrainCells.Presentation/wwwroot/js/site.js
@@ -1,1 +1,16 @@
-﻿
+﻿function showPassword() {
+
+    const passwordInput = document.getElementById('password');
+
+    passwordInput.type = 'text';
+
+}
+
+
+function hidePassword() {
+
+    const passwordInput = document.getElementById('password');
+
+    passwordInput.type = 'password';
+
+}
