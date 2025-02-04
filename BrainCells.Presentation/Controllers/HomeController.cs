dc0c41ee10fa9677597e9e0acd4ef648683fa7cf
Hamed Damavandi <160993016+fault3r@users.noreply.github.com>
2025-02-04 09:02:17 +0000

    initial home controller

--- BrainCells.Presentation/Controllers/HomeController.cs
+++ BrainCells.Presentation/Controllers/HomeController.cs
@@ -2,7 +2,6 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BrainCells.Presentation.Models;
 using Microsoft.AspNetCore.Authorization;
-using BrainCells.Application.Common;
 
 namespace BrainCells.Presentation.Controllers;
 
@@ -19,12 +18,7 @@ public class HomeController : Controller
     [Authorize]
     [HttpGet]
     public IActionResult Index()
-    {
-        string s = OnetimePassword.Create();
-        string sa = OnetimePassword.Create();
-        string saa = OnetimePassword.Create();
-        string saaa = OnetimePassword.Create();
-        
+    {   
         _logger.LogInformation("***It's fault3r, Inc. WELCOME..0;");
         return View("Index");
     }
