50a70f04dab5ec69527933b5f6230fc064a90c67
Hamed Damavandi <160993016+fault3r@users.noreply.github.com>
2025-02-09 12:37:20 +0000

    AppResource edit

--- BrainCells.Application/Common/AppResources.cs
+++ BrainCells.Application/Common/AppResources.cs
@@ -7,15 +7,17 @@ public static class AppResources
 {
     public const string AppLogo = "app-logo.png";
     public const string ProfilePicture = "profile-picture.png";
+
     public const string EmailTemplate = "otp-template.html";
  
     public static MemoryStream GetResource(IWebHostEnvironment webHost, string resource)
     {
         string path = Path.Combine(webHost.WebRootPath, "resource", resource);
-        FileStream file = new FileStream(path, FileMode.Open);
         MemoryStream memory = new();
-        file.CopyTo(memory);
-        file.Close();
+        using(FileStream file = new FileStream(path, FileMode.Open))
+        {
+            file.CopyToAsync(memory);
+        }
         memory.Close();
         return memory;
     }
